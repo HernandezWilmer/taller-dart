1d0cc9b99c976f3dbd6e875ebae2017698ce5542
Wilmer H <wilmer.hernandez@gmail.com>
2021-04-10 11:55:48 -0500

    Concatenando

--- 2_TiposDeDatos/tipos.dart
+++ 2_TiposDeDatos/tipos.dart
@@ -5,12 +5,24 @@ void main() {
   int edad = 52;
   double estatura = 1.80;
 
-  print(estatura);
+  //print(estatura);
 
   if (edad.isNegative) {
     print("Este número es negativo.");
-    print(edad.abs());
+    //print(edad.abs());
   } else {
-    print(edad.abs());
+    //print(edad.abs());
   }
+
+  //Strings
+
+  var texto1 = 'Las comillas simples funcionan bien para cadenas literales';
+  var texto2 = "Las comillas dobles funcionan bien para cadenas literales";
+
+  //Concatenar
+  var texto6 = 'valor1' + 'valor2';
+  var texto7 = 'valor1' 'valor2';
+
+//  print(texto6);
+  print(texto7);
 }
