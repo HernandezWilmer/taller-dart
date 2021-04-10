a94c93f831a43464d3525e17e571b837b872765f
Wilmer H <wilmer.hernandez@gmail.com>
2021-04-10 14:30:47 -0500

    Funciones2

--- 3_Funciones/funciones.dart
+++ 3_Funciones/funciones.dart
@@ -14,5 +14,6 @@ String obtenerNombre(String valor) {
 
 String obtenerNombreDos(String valor) => 'Mi Nombre es $valor';
 
-String obtenerNombreTres({String nombre = 'Pablo', int? edad}) =>
-    'Mi Nombre es $nombre y mi edad es $edad';
+String obtenerNombreTres({String nombre = 'Pablo', int? edad}) {
+  return 'Mi Nombre es $nombre y mi edad es $edad';
+}
